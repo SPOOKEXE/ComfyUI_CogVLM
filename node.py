e74160c1685f3a737cd519feb8db062580e3fe79
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2024-12-25 01:54:20 +0930

    edits

--- node.py
+++ node.py
@@ -8,9 +8,11 @@ sys.path.append(
 
 from transformers import AutoModelForCausalLM, LlamaTokenizer
 from PIL import Image
+from torchvision import transforms
 
 import torchvision.transforms as T
 import torch
+import numpy as np
 
 def caption_cogvlm(image, max_new_tokens):
     print("Captioning image with CogVLM")
@@ -22,27 +24,26 @@ def caption_cogvlm(image, max_new_tokens):
         trust_remote_code=True
     ).to('cuda').eval()
 
+    # Process the image
     image = image.squeeze(0).permute(2, 0, 1)
     image = T.ToPILImage()(image)
 
+    print(image)
+
     query = 'Describe the content of the image in a clear and concise sentence, focusing on the main objects, actions, and context, while maintaining natural language fluency'
-
-    try:
-        inputs = model.build_conversation_input_ids(tokenizer, query=query, history=[], images=[image])
-        print("Generated inputs:", inputs)
-
-        if 'input_ids' not in inputs:
-            raise ValueError("Missing 'input_ids' in the input dictionary")
-
-        gen_kwargs = {"max_new_tokens": max_new_tokens, "do_sample": False}
-        with torch.no_grad():
-            outputs = model.generate(inputs['input_ids'], **gen_kwargs)
-            outputs = outputs[:, inputs['input_ids'].shape[1]:]
-        print("Finished captioning image with CogVLM")
-        return tokenizer.decode(outputs[0])
-    except Exception as e:
-        print("Error during captioning:", str(e))
-        raise
+    inputs = model.build_conversation_input_ids(tokenizer, query=query, history=[], images=[image])
+    inputs = {
+        'input_ids': inputs['input_ids'].unsqueeze(0).to('cuda'),
+        'token_type_ids': inputs['token_type_ids'].unsqueeze(0).to('cuda'),
+        'attention_mask': inputs['attention_mask'].unsqueeze(0).to('cuda'),
+        'images': [[inputs['images'][0].to('cuda').to(torch.bfloat16)]],
+    }
+    gen_kwargs = {"max_new_tokens": max_new_tokens, "do_sample": False}
+    
+    with torch.no_grad():
+        outputs = model.generate(**inputs, **gen_kwargs)
+        outputs = outputs[:, inputs['input_ids'].shape[1]:]
+    return outputs
 
 class CogVLMCaption:
     @classmethod
